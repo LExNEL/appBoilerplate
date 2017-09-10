5baa9e572701a16c7187740f465927b4abc1eac4
LEx <lex.klimin@gmail.com>
2017-09-10 17:48:40 +0700

    (i) Initial configuration of the project

---
+++ source/app.js
@@ -0,0 +1,6 @@
+import './app.styl'
+
+console.log('App test')
+let h1 = document.createElement('h1')
+h1.innerText = 'App test'
+document.body.append(h1)
---
+++ webpack.config.babel.js
@@ -0,0 +1,73 @@
+import path from 'path'
+import webpack from 'webpack'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+import ExtractTextPlugin from 'extract-text-webpack-plugin'
+
+process.noDeprecation = true
+
+const NODE_ENV = process.env.NODE_ENV=='development'
+const PATHS = {
+  source: path.join(__dirname, 'source'),
+  build: path.join(__dirname, 'build')
+}
+
+export default {
+  entry: {
+      app: `${PATHS.source}/app.js`,
+  },
+  output: {
+      path: PATHS.build,
+      filename: 'js/[name].js',
+  },
+  module: {
+    rules: [{
+      test: /\.pug$/,
+      loader: 'pug-loader',
+      options: {
+        pretty: true
+      }
+    }, {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader'
+    }, {
+      test: /\.styl$/,
+      use: ExtractTextPlugin.extract({
+        // publicPath: '../',
+        fallback: 'style-loader',
+        use: ['css-loader', 'stylus-loader'],
+      }),
+    }, {
+      test: /\.css$/,
+      use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: 'css-loader',
+      }),
+    }, {
+      test: /\.(png|jpg|gif|svg)$/,
+      loader: 'file-loader',
+      options: {
+        name: '[name].[ext]?[hash]'
+      }
+    }]
+  },
+  devServer: {
+    // stats: 'errors-only'
+  },
+  devtool: (NODE_ENV) ? 'eval' : 'source-map',
+  plugins: [
+    new ExtractTextPlugin({
+      filename: 'css/[name].css',
+      allChunks: true,
+      disable: NODE_ENV
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      chunks: ['app', 'common'],
+      template: `${PATHS.source}/index.pug`
+    }),
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'common'
+    })
+  ]
+}
