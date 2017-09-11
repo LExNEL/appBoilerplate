99248393c75233b3d4c4292b55ca6279f09f2aa0
LEx <lex.klimin@gmail.com>
2017-09-11 22:12:50 +0700

    (m) Changing the webpack configuration

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,14 +1,15 @@
 import path from 'path'
+import webpack from 'webpack'
+import ExtractTextPlugin from 'extract-text-webpack-plugin'
 
 process.noDeprecation = true
 
-const NODE_ENV = process.env.NODE_ENV == 'development'
 const PATHS = {
   source: path.join(__dirname, 'src'),
   build: path.join(__dirname, 'dist')
 }
 
-export default {
+const COMMON = {
   entry: {
     main: `${PATHS.source}/main.js`,
   },
@@ -35,10 +36,17 @@ export default {
       loader: 'babel-loader'
     }, {
       test: /\.styl$/,
-      use: ['style-loader', 'css-loader', 'stylus-loader'],
+      use: ExtractTextPlugin.extract({
+        publicPath: '../',
+        fallback: 'style-loader',
+        use: ['css-loader', 'stylus-loader'],
+      })
     }, {
       test: /\.css$/,
-      use: ['style-loader', 'css-loader'],
+      use: ExtractTextPlugin.extract({
+        fallback: 'style-loader',
+        use: 'css-loader',
+      })
     }, {
       test: /\.(png|jpg|gif|svg)$/,
       loader: 'file-loader',
@@ -46,9 +54,50 @@ export default {
         name: '[name].[ext]?[hash]'
       }
     }]
-  },
+  }
+}
+
+const DEV = {
   devServer: {
     stats: 'errors-only'
   },
-  devtool: (NODE_ENV) ? 'eval' : 'source-map',
+  devtool: 'eval',
+  plugins: [
+    new ExtractTextPlugin({
+      disable: true
+    })
+  ]
+}
+
+const PROD = {
+  plugins: [
+    new ExtractTextPlugin({
+      filename: 'css/[name].css',
+      allChunks: true,
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      compress: {
+        warnings: false,
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    })
+  ]
+}
+
+export default (env) => {
+  if (env.production)
+    return Object.assign(
+      {},
+      COMMON,
+      PROD
+    )
+  if (env.development)
+    return Object.assign(
+      {},
+      COMMON,
+      DEV
+    )
 }
